771cc2026a2f8bf6784a1ae60e31cc347654a811
Dmytro Borovych <s18963@pjwstk.edu.pl>
2020-03-06 13:46:08 +0100

    started task 3

---
+++ tut1/tut1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace tut1
+{
+    class Program
+    {
+        static async Task Main(string[] args) 
+        {
+            if(args[0] == null)
+            {
+                throw new Exception("You didn't ini")
+            }
+
+
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(args[0]);
+
+            var regex = new Regex(@"[a-z][A-Z]@");
+
+            var content = response.Content;
+            var matches = regex.Matches(content.ToString());
+
+            foreach ( var match in matches)
+            {
+                WriteLine(match.ToString());
+            }
+
+        }
+    }
+}
